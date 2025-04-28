a21a346c14a5efcbfd35992bdec4202c1d002315
VEGA <giantwhitedeer@gmail.com>
2025-04-28 18:47:19 -0400

    onion things added

---
+++ impossible-enviro-spring2025/Assets/testScenes/brain&PeelTest/pixelOnionMan/OnionManInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnionManInput : MonoBehaviour
+{
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+}
