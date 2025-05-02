6e990e0416a3c80f69a7119a931c4e1c28741459
VEGA <giantwhitedeer@gmail.com>
2025-05-02 15:54:35 -0400

    flashing light

---
+++ impossible-enviro-spring2025/Assets/testScenes/brain&PeelTest/GlowingPortkey.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using UnityEngine;
+
+public class GlowingPortkey : MonoBehaviour
+{
+    public float flashDuration = 2f; 
+    public float maxIntensity = 8f;
+    public float minIntensity = 0f;
+
+    private Light targetLight;
+
+    void Start()
+    {
+        targetLight = GetComponent<Light>();
+        StartCoroutine(GlowingLight());
+    }
+
+    private IEnumerator GlowingLight()
+    {
+        float t = flashDuration;
+        int direction = 1;
+
+        yield return new WaitForSeconds(5f);
+
+        while (true)
+        {
+            t += direction * Time.deltaTime;
+            if (t >= flashDuration)
+            {
+                direction = -1;
+            }
+
+            else if (t <= 0f)
+            {
+                direction = 1;
+            }
+
+            targetLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, t / flashDuration);
+            yield return null;
+
+        }
+
+    }
+
+}
+
+
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class GlowingPortkey : MonoBehaviour
+//{
+//    public float flashDuration; // how long the flash lasts
+//    public float maxIntensity = 8f; // max intensity of the light flash
+//    public float minIntensity = 0f; 
+//    private Light targetLight; // light reference... obviously
+
+//    void Start()
+//    {
+//        targetLight = GetComponent<Light>();
+//        StartCoroutine("GlowingLight");
+//    }
+
+//    void Update()
+//    {
+
+//    }
+
+//    public IEnumerator GlowingLight()
+//    {
+//       while (true)
+//        {
+//            while (targetLight.intensity <= maxIntensity) 
+//            {
+//                targetLight.intensity -= Mathf.Lerp(maxIntensity, minIntensity, flashDuration * Time.deltaTime);
+//                yield return null;
+//            }
+
+//            while (targetLight.intensity >= minIntensity)  
+//            {
+//                targetLight.intensity += Mathf.Lerp(minIntensity, maxIntensity, flashDuration * Time.deltaTime);
+//                yield return null;
+//            }
+
+//            yield return null;
+//        }
+//    }
+//}
