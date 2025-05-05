71f57d1a3e7515bbf3c47c1fcf008232ecae7ee6
elelx <elvaschoo@gmail.com>
2025-05-05 03:44:28 -0400

    hallways

--- impossible-enviro-spring2025/Assets/testScenes/mirrorScene/mirrorRoom/MirrorPlayerMovement.cs
+++ impossible-enviro-spring2025/Assets/testScenes/mirrorScene/mirrorRoom/MirrorPlayerMovement.cs
@@ -5,62 +5,58 @@ using UnityEngine;
 public class MirrorPlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 1f;
+    public float jumping = 5f;
 
 
     public float lookSpeedX = 2f;
     public float lookSpeedY = 2f;
-
-
     private float rotationX = 0f;
 
     public Camera playerCamera;
-
-    public float jumping = 5f;
     public Rigidbody rb;
 
     private bool onTheGround = true;
 
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
 
         Cursor.lockState = CursorLockMode.Locked;
 
     }
     void Update()
     {
-        if (Input.GetKey("w"))
-        {
-            transform.position += transform.forward * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.position += new Vector3(0, 0, -moveSpeed);
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.position -= transform.right * moveSpeed * Time.deltaTime;
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.position += transform.right * moveSpeed * Time.deltaTime;
-        }
 
-        if (Input.GetKey("space") && onTheGround)
-        {
-            rb.AddForce(Vector3.up * jumping, ForceMode.Impulse);
-            onTheGround = false;
-        }
-
-        //mous camer movenemnt
+    
         float mouseX = Input.GetAxis("Mouse X") * lookSpeedX;
-        transform.Rotate(Vector3.up * mouseX);
+    transform.Rotate(Vector3.up* mouseX);
 
         float mouseY = Input.GetAxis("Mouse Y") * lookSpeedY;
-        rotationX -= mouseY;
-        rotationX = Mathf.Clamp(rotationX, -80f, 80f); // Prevent camera from flipping over
+    rotationX -= mouseY;
+        rotationX = Mathf.Clamp(rotationX, -80f, 80f);
         playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && onTheGround)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); // stop any upward/downward speed
+    rb.AddForce(Vector3.up* jumping, ForceMode.Impulse);
+            onTheGround = false;
+        }
     }
+
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal"); // A/D keys
+        float moveZ = Input.GetAxis("Vertical");   // W/S keys
+        Vector3 movement = transform.forward * moveZ + transform.right * moveX;
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
 
 
     void OnCollisionEnter(Collision collision)
@@ -70,6 +66,38 @@ public class MirrorPlayerMovement : MonoBehaviour
         {
             onTheGround = true;
         }
+
+       
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("frontDoor"))
+        {
+            Animator doorAnimator = other.GetComponent<Animator>();
+            if (doorAnimator != null)
+            {
+                doorAnimator.SetTrigger("FrontDoor");
+            }
+        }
+
+        if (other.CompareTag("LDoor"))
+        {
+            Animator doorAnimator = other.GetComponent<Animator>();
+            if (doorAnimator != null)
+            {
+                doorAnimator.SetTrigger("leftDoor");
+            }
+        }
+
+        if (other.CompareTag("RDoor"))
+        {
+            Animator doorAnimator = other.GetComponent<Animator>();
+            if (doorAnimator != null)
+            {
+                doorAnimator.SetTrigger("rightDoor");
+            }
+        }
+        
+    }
 }
