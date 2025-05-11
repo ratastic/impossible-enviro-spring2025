3a87ebd422ab53a39de00cba4dd48d4f9bd46f80
silviayq <150416252+silviayq@users.noreply.github.com>
2025-05-11 04:17:10 -0500

    make palms floating

---
+++ impossible-enviro-spring2025/Assets/testScenes/nerveTreeScene/FixedCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    // 设置摄像机的固定位置
+    public Vector3 fixedPosition;
+
+    void Start()
+    {
+        // 在启动时将摄像机固定在特定位置
+        transform.position = fixedPosition;
+    }
+
+    void Update()
+    {
+        // 摄像机保持固定位置，不随物体移动
+        transform.position = fixedPosition;
+    }
+}
---
+++ impossible-enviro-spring2025/Assets/testScenes/nerveTreeScene/FloatingObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class FloatingObject : MonoBehaviour
+{
+    // 控制浮动的幅度
+    public float floatHeight = 1.0f;
+    // 控制浮动速度
+    public float floatSpeed = 1.0f;
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+        // 记录物体初始位置
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        // 使用 Mathf.PingPong 来实现上下浮动效果
+        float newY = startPos.y + Mathf.PingPong(Time.time * floatSpeed, floatHeight);
+        transform.position = new Vector3(startPos.x, newY, startPos.z);
+    }
+}
